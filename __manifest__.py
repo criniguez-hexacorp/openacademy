52eb3fdf236da14589c366634c94d7d52a0d46f4
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-03-05 20:17:20 +0000

    relations between models

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/course.xml',
+        'views/session.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
