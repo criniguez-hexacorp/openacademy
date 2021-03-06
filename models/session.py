00ae674fc439300d657fc6b4a0744a74e3935328
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-03-06 15:18:18 +0000

    advanced views: tree views

--- models/session.py
+++ models/session.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, exceptions
 
 
 class Session(models.Model):
@@ -55,3 +55,11 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees"
                 }
             }
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(
+                    "A session's instructor can't be an attendee"
+                )
