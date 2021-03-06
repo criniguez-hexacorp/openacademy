4e0f81f8e63ed9352fb59f47d085e714c4b30682
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-03-06 13:52:26 +0000

    relations between models: relational fields

--- models/course.py
+++ models/course.py
@@ -1,3 +1,4 @@
+from operator import index
 from odoo import api, fields, models
 
 
@@ -7,3 +8,15 @@ class Course(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
+
+    responsible_id = fields.Many2one(
+        'res.users',
+        ondelete='set null',
+        string="Responsible",
+        index=True
+    )
+    session_ids = fields.One2many(
+        'openacademy.session',
+        'course_id',
+        string="Sessions"
+    )
