7bbf0d4d3d0274a01dd36e5eefacd42670ac4305
criniguez-hexacorp <cristian.iniguez@hexacorp.com.bo>
2021-03-08 15:02:40 +0000

    web-services: xml-rpc library

---
+++ web-services/xml-rpc_lib.py
@@ -0,0 +1,28 @@
+import functools
+import xmlrpc.client
+
+HOST = 'localhost'
+PORT = 8069
+DB = 'saitolab'
+USER = 'admin'
+PASS = 'admin123'
+ROOT = 'http://%s:%d/xmlrpc/' % (HOST, PORT)
+
+# 1. Login
+uid = xmlrpc.client.ServerProxy(ROOT + 'common').login(DB, USER, PASS)
+print("Logged in as %s (uid:%d)" % (USER, uid))
+
+call = functools.partial(
+    xmlrpc.client.ServerProxy(ROOT + 'object').execute,
+    DB, uid, PASS)
+
+# 2. Read the sessions
+sessions = call('openacademy.session', 'search_read', [], ['name', 'seats'])
+for session in sessions:
+    print("Session %s (%s seats)" % (session['name'], session['seats']))
+
+# 3.create a new session
+session_id = call('openacademy.session', 'create', {
+    'name': 'My session',
+    'course_id': 2,
+})
